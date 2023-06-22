649a9e5b8cc578cde831630396a6d073a6182972
Rider Echeverria <ryder.echerz@gmail.com>
2023-06-21 22:12:55 -0400

    tab4_Hproducto

--- src/app/servicios-backend/hproducto/hproducto.service.ts
+++ src/app/servicios-backend/hproducto/hproducto.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HproductoService {
 
-  constructor() { }
+  PATH_BACKEND = environment.URL_BACKEND + environment.PORT_BACKEND;
+
+  URL_GET_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto"
+  URL_GET_BY_ID = this.PATH_BACKEND + "/api/HProducto/GetHProductoById"
+  URL_ADD_HPRODUCTO = this.PATH_BACKEND + "/api/HProducto/AddHProducto"
+
+  
+
+  constructor(private http: HttpClient) { }
+
+  public GetHProducto(): Observable<HttpResponse<any>> 
+  {
+    return this.http
+      .get<any>(this.URL_GET_HPRODUCTO,
+        { observe: 'response' })
+      .pipe();
+  }
+
+  public AddHProducto(entidad): Observable<HttpResponse<any>> 
+  {
+    return this.http
+      .post<any>(this.URL_ADD_HPRODUCTO, entidad,
+        { observe: 'response' })
+      .pipe();
+  }
+
 }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { HproductoService } from '../servicios-backend/hproducto/hproducto.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
 })
-export class Tab4Page implements OnInit {
+export class Tab4Page{
 
-  constructor() { }
+  public listHProducto = [];
+  public idHProducto = ""
+  public cantidad = ""
+  public IdProducto = ""
+  public IdCarritoCompra = ""
+  public swGuardarCambios = false
 
-  ngOnInit() {
+  constructor(private hproductoService: HproductoService) 
+  {
+    this.GetHProducto();//Se carga el listado cada vez que se abra la pag.
   }
+
+  public GetHProducto()
+  {
+    this.hproductoService.GetHProducto().subscribe
+    ({
+      next:(response: HttpResponse<any>)=>
+      {
+        this.listHProducto = response.body;
+      },
+      error: (error: any) => 
+      {
+        console.log(error);
+      },
+      complete: () => 
+      {
+        console.log('complete - this.GetHProducto()');
+      },
+    });
+  }
+
+  public addHProducto()
+  {
+    if (this.cantidad.length > 0 && this.IdProducto.length > 0 && this.IdCarritoCompra.length > 0) 
+    {
+      var entidad = 
+      {
+        cantidad : this.cantidad,
+        IdProducto : this.IdProducto,
+        IdCarritoCompra : this.IdCarritoCompra
+      }
+      console.log(entidad)
+      this.hproductoService.AddHProducto(entidad).subscribe
+      ({
+          next: (response: HttpResponse<any>) => 
+          {
+            console.log(response.body)//1
+            if(response.body == 1)
+            {
+              alert("Se agrego el Detalle del Producto con exito :)");
+              this.GetHProducto();//Se actualize el listado
+              this.cantidad = "";
+              this.IdProducto = "";
+              this.IdCarritoCompra = "";
+            }
+            else
+            {
+              alert("Al agregar el Producto fallo :(");
+            }
+          },
+          error: (error: any) => 
+          {
+            console.log(error);
+          },
+          complete: () => 
+          {
+            console.log('complete - this.addProducto()');
+          },
+      });
+    }
+  }
+
+
 
 }
